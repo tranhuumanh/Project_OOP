dab3965fc37bd184318da7b7ccba0a1778350a39
gadu04 <seinarugaru@gmail.com>
2023-11-23 15:54:47 +0700

    aaa

---
+++ src/Dictionary.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+    private ArrayList<Word> words = new ArrayList<>();
+
+    public ArrayList<Word> getWords() {
+        return words;
+    }
+}
