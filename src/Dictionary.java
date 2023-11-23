a1d94abda3d48c0a443c6bf6ca35abfd0e1f01eb
gadu04 <seinarugaru@gmail.com>
2023-11-23 19:21:13 +0700

    aaa

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Dictionary {
-    private ArrayList<Word> words = new ArrayList<>();
+    public static ArrayList<Word> words = new ArrayList();
 
     public ArrayList<Word> getWords() {
         return words;
--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,14 +1,8 @@
 import java.util.Scanner;
 
 public class DictionaryManagement {
-    private Word[] words; // mảng các từ
-    private int size; // số lượng từ
 
-    // Hàm khởi tạo
-    public DictionaryManagement() {
-        words = new Word[1000]; // giả sử có tối đa 1000 từ
-        size = 0; // ban đầu chưa có từ nào
-    }
+    private int size; // số lượng từ
 
     // Hàm nhập liệu từ bàn phím
     public void insertFromCommandline() {
@@ -16,13 +10,14 @@ public class DictionaryManagement {
         System.out.println("Nhập số lượng từ vựng:");
         int n = sc.nextInt(); // đọc số lượng từ vựng
         sc.nextLine(); // đọc bỏ dòng trống
+        size = n; // Initialize size with the number of words
         for (int i = 0; i < n; i++) {
             System.out.println("Nhập từ tiếng Anh:");
             String word_target = sc.nextLine(); // đọc từ tiếng Anh
             System.out.println("Nhập giải thích bằng tiếng Việt:");
             String word_explain = sc.nextLine(); // đọc giải thích tiếng Việt
-            words[size] = new Word(word_target, word_explain); // tạo đối tượng Word và thêm vào mảng
-            size++; // tăng số lượng từ lên 1
+            Word worded = new Word(word_target, word_explain); // tạo đối tượng Word và thêm vào mảng
+            Dictionary.words.add(worded);
         }
         sc.close(); // đóng đối tượng Scanner
     }
@@ -31,8 +26,7 @@ public class DictionaryManagement {
     public void showAllWords() {
         System.out.println("Danh sách các từ:");
         for (int i = 0; i < size; i++) {
-            System.out.println(words[i].getWord_target() + " : " + words[i].getWord_explain());
+            System.out.println(Dictionary.words.get(i).getWord_target() + " : " + Dictionary.words.get(i).getWord_explain());
         }
     }
-
 }
