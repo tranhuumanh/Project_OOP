c51aa818bf9eda6d3f7abecf9d30453541d0cf7a
Manh <manhtran1342@gmail.com>
2023-11-24 16:53:46 +0700

    Translate

--- src/TranslateAPIGG.java
+++ src/TranslateAPIGG.java
@@ -27,8 +27,7 @@ public class TranslateAPIGG {
     }
 
     public static void main(String[] args) throws IOException {
-        Scanner input = new Scanner(System.in);
-        String text = input.nextLine();
+        String text = "hello";
         System.out.println("Translated text: \n" + googleTranslate("", "vi", text));
     }
 
