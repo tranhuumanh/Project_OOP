9a62651d8de8cdbaa9f7f14c1af6243ad0bd566a
gadu04 <126551507+gadu04@users.noreply.github.com>
2023-11-24 18:19:49 +0700

    Add files via upload

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -3,9 +3,15 @@ import java.util.ArrayList;
 public class Dictionary {
     public static ArrayList<Word> words = new ArrayList();
 
+    public static ArrayList<String> SaveHistoryWord = new ArrayList();
+
     public ArrayList<Word> getWords() {
         return words;
     }
 
-    public ArrayList<>
+    public ArrayList<String> getSaveHistoryWord() {
+        return SaveHistoryWord;
+
+    }
+
 }
